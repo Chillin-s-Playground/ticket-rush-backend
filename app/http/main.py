c2858a14abc46c3d9d031cf1bbfe020c93eec936
chilln <kimjihan77@naver.com>
2025-08-15 11:37:12 +0900

    fix:CORS 에러 수정

--- app/http/main.py
+++ app/http/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.exception import UnknownException, exception_handler
 from app.http.health import router as health
 from app.http.ticket import router as ticket
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(health)
 app.include_router(ticket)
