51e37dc86957ea3b15718a5a288c63818e1ace3b
chilln <kimjihan77@naver.com>
2025-08-15 23:32:19 +0900

    fix:토큰 유효성 검증 함수에서 Exception 처리 위임

--- app/core/token.py
+++ app/core/token.py
@@ -1,6 +1,6 @@
 # 약간 뭐라해야하지 비로그인 방식이라 auth는 아니고, token이 좀 더 맞는 명칭 같아서 token.py 파일이라고 명명함.
 import redis
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 
 from app.core.redis import get_redis
 
@@ -10,7 +10,9 @@ async def verify_token(
 ) -> str | None:
     """토큰 유효성 검사"""
 
-    token = await redis.get(f"gate:token:{token}")
+    vaild_token = await redis.get(f"gate:token:{token}")
 
-    if token:
-        return token
+    if not vaild_token:
+        raise HTTPException(status_code=401, detail="Invalid Token!")
+
+    return vaild_token
