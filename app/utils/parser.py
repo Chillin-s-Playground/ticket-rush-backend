6ab656457887545215b342442d6c0edd8168e95b
chilln <kimjihan77@naver.com>
2025-08-16 18:53:50 +0900

    feat:좌석 상태 변경 payload 생성 메소드

--- app/utils/parser.py
+++ app/utils/parser.py
@@ -21,3 +21,18 @@ def build_seat_update_payload(type: str, event_id: int, seat_label: str) -> dict
         }
     else:
         return None
+
+
+def build_payload(*seats: tuple[int, str], with_prev=False):
+    """좌석 상태 변경 payload 생성"""
+    payload = []
+    for seat in seats:
+        seat_id, seat_status = seat
+        key = "seat_id" if not with_prev else "prev_seat_id"
+        payload.append(
+            {
+                key if seat == seats[-1] and with_prev else "seat_id": seat_id,
+                f"{'prev_' if with_prev and seat == seats[-1] else ''}seat_status": seat_status,
+            }
+        )
+    return payload
